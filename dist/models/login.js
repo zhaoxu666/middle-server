eaff8d906ebe560b6df4f7eaf303befc59513289
victor <cbcbj.zhaoxu@cbcbeijing.net>
2020-12-04 16:30:16 +0800

    rm dist floder

--- dist/models/login.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserInfo {
-    constructor(username, password) {
-        this.username = username;
-        this.password = password;
-    }
-}
-exports.default = UserInfo;
